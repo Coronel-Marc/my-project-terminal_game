0340e5a88265c910b9d87ca1afdf2595660bd2ac
Marcos Gabriel <jorgede1097@gmail.com>
2025-07-23 20:39:48 -0300

    I created a class to encapsulate all the game logic

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import entities.Enemy;
+import entities.Entity;
 import entities.Player;
 
 import java.util.Scanner;
@@ -6,44 +7,76 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
+
+        /*
+        * I'm trying to create a new player using Scanner class.
+        * */
+
+
+        gameLoop();
+
+
+
+    }
+
+    public static void gameLoop(){
         Scanner scanner = new Scanner(System.in);
 
+
         Player player = new Player(
-                "Jon",
+                "unknown",
                 100,
                 100,
                 10
         );
         Enemy enemy = new Enemy(
                 "Orc",
-                100,
-                100,
-                10
+                80,
+                50,
+                7
         );
 
-        System.out.println(player.greeting());
-        System.out.println(enemy.greeting());
-        System.out.println(player.toPunch(enemy));
+        System.out.print("Welcome to my terminal game.\nChoose your name: ");
+        player.setName(scanner.nextLine());
+        System.out.println("Greetings, " + player.getName());
 
         boolean inLoop = true;
-        /*
-        * I'm trying to create a new player using Scanner class.
-        * */
         while (inLoop){
-            Player player2 = new Player();
-            System.out.print("Welcome to my terminal game.\nChoose your name: ");
-            player2.setName(scanner.nextLine());
-            System.out.println(player2.getName());
-
-            inLoop = false;
+            System.out.println("Choose an option: ");
+            System.out.println(
+                    """
+                    1 - Check name
+                    2 - Check enemy
+                    3 - Check your status
+                    0 - Exit
+                    """
+            );
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println(player.getName());
+                    break;
+                case 2:
+                    System.out.println(enemy.getName());
+                    break;
+                case 3:
+                    System.out.println("health: " + player.getHealth()+"\nStamina: "+player.getStamina());
+                    break;
+                case 0:
+                    System.out.println("Closing game...bye");
+                    inLoop = false;
+                    break;
+                default:
+                    System.out.println("Wrong choice bro...");
+            }
         }
-
-
+        scanner.close();
     }
 }
 
 class gameLoop{
-    boolean inLoop = false;
+
 
 
 }
